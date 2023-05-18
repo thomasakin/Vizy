dd8899821e0bd8a359c5a3fea538c5df7206508a
T Akin <thomas.akin@tanium.com>
2023-05-18 14:13:37 -0400

    Initial Commit

---
+++ Vizy/VizyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VizyApp.swift
+//  Vizy
+//
+//  Created by Thomas Akin on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct VizyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
