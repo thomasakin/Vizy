3ef6bb8a63a0842d786ff4e9f83b1ee6172928bd
Thomas Akin <thomas.akin@tanium.com>
2023-08-04 18:07:54 -0400

    Colors

--- Vizy/Bindings+Extenstions.swift
+++ Vizy/Bindings+Extenstions.swift
@@ -69,18 +69,53 @@ extension CoreDataTask {
     // }
 }
 
+extension Color {
+    static let todoColor = Color(hex: "#FEC601")
+    static let doingColor = Color(hex: "#3DA5D9")
+    static let doneColor = Color(hex: "#73BFB8")
+    static let pastDueFontColor = Color(hex: "#000000")
+    static let dueTodayFontColor = Color(hex: "#FFFFFF")
+    static let futureDueFontColor = Color(hex: "#737373")
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
+
 func dueDateColor(for date: Date, state: TaskState) -> Color {
     let today = Calendar.current.startOfDay(for: Date())
     let dueDate = Calendar.current.startOfDay(for: date)
 
-    if dueDate < today && state != .done {
-        return Color(red: 232/255, green: 65/255, blue: 24/255).opacity(0.75)
+    if dueDate < today {
+        return Color.pastDueFontColor.opacity(0.75)
     } else if Calendar.current.isDateInToday(dueDate) {
-        return Color(red: 156/255, green: 136/255, blue: 255/255).opacity(0.75)
+        return Color.dueTodayFontColor.opacity(0.75)
     } else if dueDate > today {
-        return Color(red: 245/255, green: 246/255, blue: 250/255).opacity(0.75)
-    } else if state == .done {
-        return Color(red: 220/255, green: 221/255, blue: 225/255).opacity(0.75)
+        return Color.futureDueFontColor.opacity(0.75)
     } else {
         return Color.primary.opacity(0.75)
     }
